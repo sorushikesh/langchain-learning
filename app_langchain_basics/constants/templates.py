7d28cd0cc9daab754ca96a2608759847e01cb0b6
sorushikesh <sorushikesh07@gmail.com>
2025-06-06 09:58:31 +0530

    Langchain basics: Chaining concept

---
+++ app_langchain_basics/constants/templates.py
@@ -0,0 +1,10 @@
+class Templates:
+    """
+    Class to store all templates
+    """
+    
+    summary_template = (
+        "Gives the information about the person {information}. From this, I want you to create:"
+        "1. A short summary"
+        "2. Some interesting facts about them"
+    )
