e01e0a95b8e57bdb2e9a34958376db110a94de4c
Rushikesh <sorushikesh07@gmail.com>
2025-08-15 01:07:22 +0530

    LangChain: Chains in langchain

---
+++ basics/chat_model_conversation_with_user.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_openai import AzureChatOpenAI
+
+from app.constants.config import ModelDetails
+
+llm = AzureChatOpenAI(
+    azure_endpoint=ModelDetails.AZURE_ENDPOINT,
+    azure_deployment=ModelDetails.AZURE_DEPLOYMENT_NAME,
+    api_key=ModelDetails.AZURE_API_KEY,
+    api_version=ModelDetails.AZURE_API_VERSION,
+    temperature=1
+)
+
+chat_history = []
+system_message = SystemMessage(content="You are an expert in geo-politics")
+chat_history.append(system_message)
+
+while True:
+    query = input("User input : ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))
+
+    result = llm.invoke(chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content=response))
+    print(f"AI response {response}")
+
+print(f"Chat history : {chat_history}")
---
+++ basics/sequential_chain_multi_step_chain.py
@@ -0,0 +1,58 @@
+from langchain.chains.llm import LLMChain
+from langchain.chains.sequential import SequentialChain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import AzureChatOpenAI
+
+from app.constants.config import ModelDetails
+
+llm = AzureChatOpenAI(
+    azure_endpoint=ModelDetails.AZURE_ENDPOINT,
+    azure_deployment=ModelDetails.AZURE_DEPLOYMENT_NAME,
+    api_key=ModelDetails.AZURE_API_KEY,
+    api_version=ModelDetails.AZURE_API_VERSION,
+    temperature=1
+)
+
+title_prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="Generate 3 creative blog post titles about {topic}"
+)
+title_chain = LLMChain(
+    llm=llm,
+    prompt=title_prompt,
+    output_key="title"
+)
+
+content_prompt = PromptTemplate(
+    input_variables=["title"],
+    template="Write a short and engaging blog post for title: {title}"
+)
+content_chain = LLMChain(
+    llm=llm,
+    prompt=content_prompt,
+    output_key="content"
+)
+
+summary_prompt = PromptTemplate(
+    input_variables=["title", "content"],
+    template="Write short summary for title {title} and content {content}"
+)
+summary_chain = LLMChain(
+    llm=llm,
+    prompt=summary_prompt,
+    output_key="summary"
+)
+
+multi_chain = SequentialChain(
+    chains=[title_chain, content_chain, summary_chain],
+    input_variables=["topic"],
+    output_variables=["title", "content", "summary"],
+    verbose=True
+)
+
+if __name__ == "__main__":
+    topic = "artificial intelligence in healthcare"
+    result = multi_chain.invoke({"topic": topic})
+    print(f"Title : {result["title"]}")
+    print(f"Content : {result["content"]}")
+    print(f"Summary : {result["summary"]}")
